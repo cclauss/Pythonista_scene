6d68f3d0b035a0ec1d8da90b69dd7bf9e4858b3f
cclauss <cclauss@bluewin.ch>
2014-07-31 19:18:54 +0200

    Update SceneViewer.py

--- SceneViewer.py
+++ SceneViewer.py
@@ -5,7 +5,8 @@ class MyScene (scene.Scene):
         scene.background(0, 0, 0)
         scene.fill(1, 0, 0)
         for touch in self.touches.values():
-            scene.ellipse(touch.location.x - 50, touch.location.y - 50, 100, 100)
+            x, y = touch.location
+            scene.ellipse(x - 50, y - 50, 100, 100)
 
 class SceneViewer(ui.View):
     def __init__(self, in_scene):
@@ -13,7 +14,7 @@ class SceneViewer(ui.View):
         scene_view = scene.SceneView(frame=self.frame)
         scene_view.scene = in_scene()
         self.add_subview(scene_view)
-        self.close_button()
+        self.add_subview(self.close_button())
     
     def close_action(self, sender):
         print('Closing...')
@@ -21,11 +22,11 @@ class SceneViewer(ui.View):
 
     def close_button(self):
         the_button = ui.Button(title='X')
-        the_button.border_width = 3
-        the_button.border_color = 'blue'
+        the_button.x = self.width - the_button.width
+        the_button.y = the_button.height / 2
         the_button.action = self.close_action
-        the_button.font=('<system-bold>', 48)
-        self.add_subview(the_button)
+        the_button.font=('<system-bold>', 20)
+        return the_button
 
 #scene.run(MyScene)
 SceneViewer(MyScene)
